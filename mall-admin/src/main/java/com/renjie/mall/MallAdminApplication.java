3ba770a1de5221a7b042b78b6be33b63b17e7f4d
Tuibo <402128964@qq.com>
2021-03-30 23:45:11 +0800

    修改配置文件

--- mall-admin/src/main/java/com/renjie/mall/MallAdminApplication.java
+++ mall-admin/src/main/java/com/renjie/mall/MallAdminApplication.java
@@ -1,9 +1,11 @@
 package com.renjie.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.renjie.mall.dao")
 public class MallAdminApplication {
 
 	public static void main(String[] args) {
