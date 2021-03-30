828a589e1aeaadfbcb40177ed186ca3e1697922d
Tuibo <402128964@qq.com>
2021-03-30 23:51:10 +0800

    修改配置文件

---
+++ mall-admin/src/test/java/com/renjie/mall/TestController.java
@@ -0,0 +1,11 @@
+package com.renjie.mall;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Author gjc
+ * @Date 2021/3/30 23:50
+ * @Version 1.0
+ **/
+public class TestController {
+}
